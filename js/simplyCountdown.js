d6ecc97d7b3425d9ee7b70c4916f19c67024cbc3
Jan Povala <jan.povala11@imperial.ac.uk>
2025-01-22 23:46:32 +0000

    Fixing typos and fixing the countdown.

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,10 +115,10 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
+                year: 2025,
                 month: 6,
-                day: 28,
-                hours: 0,
+                day: 27,
+                hours: 17,
                 minutes: 0,
                 seconds: 0,
                 words: {
@@ -155,7 +155,7 @@
 
         targetTmpDate = new Date(
             parameters.year,
-            parameters.month - 1,
+            parameters.month,
             parameters.day,
             parameters.hours,
             parameters.minutes,
